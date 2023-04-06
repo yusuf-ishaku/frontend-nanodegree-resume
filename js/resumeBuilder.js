63c1bbe145b2e6095db98a4427b007e8f739b0bf
yusuf <kingshakzy@gmail.com>
2023-04-06 13:10:03 +0100

    Model set up

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,65 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+let model = {
+    data: {
+        bio: {
+            name: String,
+            role: String,
+            contacts: {
+                mobile: String,
+                email: String,
+                github: String,
+                twitter: String, 
+                location: String,
+            },
+            welcomeMessage: String,
+            skills: [],
+            biopic: String,
+            display: () =>{
+
+            }
+        },
+        education:{
+            schools: [{
+                name: String,
+                location: String,
+                majors: [String, String,],
+                dates: String,
+                url: String,
+            }],
+            onlineCourses: [{
+                title: String,
+                school: String,
+                dates: String,
+                url: String
+            }],
+            display: ()=>{
+
+            }
+        },
+        work: {
+            jobs: [{
+                employer,
+                title,
+                location,
+                dates,
+                description
+            }],
+            display: ()=>{
+
+            }
+        },
+        projects: {
+            projects: [{
+                title,
+                dates,
+                description,
+                images
+            }],
+            display: () =>{
+
+            }
+        }
+    }
+}
